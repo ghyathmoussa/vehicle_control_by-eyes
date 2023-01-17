8baa74e89c407bc2fa64176bc132bb4bdde4a727
Ghyath Moussa <ghyathmoussa@hotmail.com>
2023-01-17 14:13:22 +0300

    remove unuseful lines

--- example.py
+++ example.py
@@ -7,8 +7,6 @@ import time
 gaze = GazeTracking()
 webcam = cv2.VideoCapture(0)
 
-
-
 serverAddressPort   = ("192.168.4.1", 9999)
 bufferSize          = 1024
 
@@ -49,14 +47,12 @@ while True:
         else:
             text = "Looking center"
             msgFromClient = "ST "
-    
-    prev_text = text
-    # print(f'prev: {prev_text}')
-    # print(f'text: {text}')
     print(count)
-    if prev_text == text:
+    if prev_text == text :
         count += 1
-    if  count == 20:
+    else:
+        count = 0
+    if  count == 10 and text != '':
         UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
         
@@ -72,37 +68,13 @@ while True:
 
         UDPClientSocket.sendto(str.encode("ST "), serverAddressPort)
         count = 0
-    else:
-        count += 0
-        # prev_text = text
-        # start_time = current_time
-    #text = "vertical_ratio: " + str(gaze.vertical_ratio()) + " horizontal_ratio: " + str(gaze.horizontal_ratio()) + " " + text
+    if  count == 10 and text != '':
+        msgFromClient = "ST "
+    prev_text = text
     cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
-    # if time.perf_counter() - start_time > 0.5:
-    #     # Some where before the loop
-    #     print('time > 0.5')
-    #     print("text iris_position = ", text)
-    #     print("prev iris_position = ", prev_text)
-        # UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-        
-        # bytesToSend = str.encode(msgFromClient)
-
-        # UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-
-        # msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-
-        # msg = "Message from Server {}".format(msgFromServer[0])
-
-        # print(msg)
-
-        # UDPClientSocket.sendto(str.encode("ST "), serverAddressPort)
-    # prev_text = text 
     left_pupil = gaze.pupil_left_coords()
     right_pupil = gaze.pupil_right_coords()
-    # print(f'framede {(time.perf_counter() - start_time)}')
-    # time.sleep(3)
-    cv2.imshow("Demo", frame)
+    cv2.imshow("Ekran", frame)
     key = cv2.waitKey(1)
     if key == ord("q"):
         break
